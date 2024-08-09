e44d4aaed09b2eddd02634f5397ad9acbf4b7d1a
zzucch <nikitamilovatskiy@gmail.com>
2024-08-09 17:56:01 +0300

    feat: add c, haskell and dhall language servers

--- nixvim/plugins/lsp.nix
+++ nixvim/plugins/lsp.nix
@@ -1,51 +1,81 @@
-{
-  programs.nixvim.plugins = {
-    lsp = {
-      enable = true;
-      keymaps = {
-        silent = true;
-        diagnostic = {
-          "<leader>k" = "goto_prev";
-          "<leader>j" = "goto_next";
+{pkgs, ...}: {
+  programs.nixvim = {
+    plugins = {
+      lsp = {
+        enable = true;
+        keymaps = {
+          silent = true;
+          diagnostic = {
+            "<leader>k" = "goto_prev";
+            "<leader>j" = "goto_next";
+          };
+          lspBuf = {
+            "<leader>f" = "format";
+            K = "hover";
+            gd = "definition";
+            gD = "declaration";
+            gI = "implementation";
+            "<leader>vca" = "code_action";
+            "<leader>vrf" = "references";
+            "<leader>vrn" = "rename";
+            gt = "type_definition";
+            gi = "implementation";
+          };
         };
-        lspBuf = {
-          "<leader>f" = "format";
-          K = "hover";
-          gd = "definition";
-          gD = "declaration";
-          gI = "implementation";
-          "<leader>vca" = "code_action";
-          "<leader>vrf" = "references";
-          "<leader>vrn" = "rename";
-          gt = "type_definition";
-          gi = "implementation";
+        servers = {
+          nil-ls = {
+            enable = true;
+            settings = {
+              formatting.command = ["alejandra"];
+            };
+          };
+          lua-ls.enable = true;
+          bashls.enable = true;
+          ccls.enable = true;
+          cmake.enable = true;
+          gopls.enable = true;
+          gleam.enable = true;
+          dockerls.enable = true;
+          docker-compose-language-service.enable = true;
+          tsserver.enable = true;
+          rust-analyzer = {
+            enable = true;
+            installCargo = false;
+            installRustc = false;
+            settings = {
+              cargo.features = "all";
+            };
+          };
+          hls.enable = true;
+          marksman.enable = true;
+          yamlls.enable = true;
+          dhall-lsp-server.enable = true;
         };
       };
-      servers = {
-        nil-ls = {
-          enable = true;
-          settings = {
-            formatting.command = ["alejandra"];
-          };
+      conform-nvim = {
+        enable = true;
+        notifyOnError = true;
+        formattersByFt = {
+          c = ["clang-format"];
+          nix = ["alejandra"];
+          lua = ["stylua"];
+          haskell = ["ormolu"];
+          markdown = ["prettierd"];
+          yaml = [
+            "yamllint"
+            "yamlfmt"
+          ];
         };
-        lua-ls.enable = true;
-        bashls.enable = true;
-        gopls.enable = true;
-        gleam.enable = true;
-        dockerls.enable = true;
-        docker-compose-language-service.enable = true;
-        tsserver.enable = true;
-        rust-analyzer = {
-          enable = true;
-          installCargo = false;
-          installRustc = false;
-          settings = {
-            cargo.features = "all";
-          };
-        };
-        marksman.enable = true;
-        yamlls.enable = true;
       };
     };
+    extraPackages = with pkgs; [
+      alejandra
+      clang-tools
+      stylua
+      ormolu
+      prettierd
+      yamllint
+      yamlfmt
+    ];
   };
 }
