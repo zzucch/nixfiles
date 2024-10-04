0058acb6a8077cd904bfb2feefd410cbbff0587a
zzucch <nikitamilovatskiy@gmail.com>
2024-10-05 01:44:52 +0300

    fix: explicitly add devicons

--- nixvim/plugins/telescope.nix
+++ nixvim/plugins/telescope.nix
@@ -1,21 +1,24 @@
 {
-  programs.nixvim.plugins.telescope = {
-    enable = true;
-    keymaps = {
-      "<leader>pf" = "find_files";
-      "<C-p>" = "git_files";
-      "<C-f>" = "live_grep";
-    };
-    settings.defaults = {
-      set_env.COLORTERM = "truecolor";
-      file_ignore_patterns = [
-        "^.git/"
-        "^.mypy_cache/"
-        "^__pycache__/"
-        "^output/"
-        "^data/"
-        "%.ipynb"
-      ];
+  programs.nixvim.plugins = {
+    web-devicons.enable = true;
+    telescope = {
+      enable = true;
+      keymaps = {
+        "<leader>pf" = "find_files";
+        "<C-p>" = "git_files";
+        "<C-f>" = "live_grep";
+      };
+      settings.defaults = {
+        set_env.COLORTERM = "truecolor";
+        file_ignore_patterns = [
+          "^.git/"
+          "^.mypy_cache/"
+          "^__pycache__/"
+          "^output/"
+          "^data/"
+          "%.ipynb"
+        ];
+      };
     };
   };
 }
