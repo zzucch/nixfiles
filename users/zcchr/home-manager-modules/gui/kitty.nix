4f07c05e910ba6bd5a65cfa28f39775a5b0032f3
zzucch <nikitamilovatskiy@gmail.com>
2025-01-28 01:24:14 +0300

    feat: remove kitty confirmation

--- users/zcchr/home-manager-modules/gui/kitty.nix
+++ users/zcchr/home-manager-modules/gui/kitty.nix
@@ -11,6 +11,7 @@
       background_opacity = "0.5";
       foreground = "#b0b0b0";
       dynamic_background_opacity = true;
+      confirm_os_window_close = 0;
       hide_window_decorations = true;
       enable_audio_bell = false;
       "map kitty_mod+f6" = "change_font_size current 28";
