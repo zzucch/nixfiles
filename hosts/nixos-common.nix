e28614c5199351dbc815d3b72f823d99309b903e
zzucch <nikitamilovatskiy@gmail.com>
2025-01-28 00:35:15 +0300

    feat: add some fish options

--- hosts/nixos-common.nix
+++ hosts/nixos-common.nix
@@ -1,6 +1,35 @@
 {pkgs, ...}: {
   nix.settings.experimental-features = ["nix-command" "flakes"];
-  programs.fish.enable = true;
+  programs.fish = {
+    enable = true;
+    shellAbbrs = {
+      t = "tmux";
+      ta = "tmux a";
+
+      n = "nvim .";
+
+      ga = "git add -A";
+      gc = "git commit -m";
+      gca = "git commit --amend";
+      gp = "git push";
+      gs = "git status";
+      gd = "git diff";
+      gds = "git diff --staged";
+      gdss = "git diff --staged --stat";
+      gsc = "git switch -c";
+      gsm = "git switch master";
+
+      gg = "go get";
+      gmt = "go mod tidy";
+      gt = "go test ./...";
+      gtc = "go test -count=1 ./...";
+    };
+    shellInit = ''
+      set fish_greeting
+      fish_vi_key_bindings
+      bind -M insert \t accept-autosuggestion
+    '';
+  };
   environment.systemPackages = with pkgs; [
     git
     vim
--- users/zcchr/home-manager-modules/common/direnv.nix
+++ users/zcchr/home-manager-modules/common/direnv.nix
@@ -3,6 +3,7 @@
     bash.enable = true;
     direnv = {
       enable = true;
+      silent = true;
       enableBashIntegration = true;
       enableFishIntegration = true;
       nix-direnv.enable = true;
