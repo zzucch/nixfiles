1e468cde430365d4b6cbed082d3698efb0106862
zzucch <nikitamilovatskiy@gmail.com>
2025-02-12 03:52:36 +0300

    fix(kasou): return to systemd network config

--- hosts/kasou/modules/network.nix
+++ hosts/kasou/modules/network.nix
@@ -1,4 +1,22 @@
-{lib, ...}: {
-  networking.bridges.vmbr0.interfaces = ["enp7s0"];
-  networking.interfaces.vmbr0.useDHCP = lib.mkDefault true;
+{
+  systemd.network.networks."10-lan" = {
+    matchConfig.Name = ["enp7s0"];
+    networkConfig = {
+      Bridge = "vmbr0";
+    };
+  };
+  systemd.network.netdevs."vmbr0" = {
+    netdevConfig = {
+      Name = "vmbr0";
+      Kind = "bridge";
+    };
+  };
+  systemd.network.networks."10-lan-bridge" = {
+    matchConfig.Name = "vmbr0";
+    networkConfig = {
+      IPv6AcceptRA = true;
+      DHCP = "ipv4";
+    };
+    linkConfig.RequiredForOnline = "routable";
+  };
 }
