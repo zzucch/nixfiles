2f476f7bcc5c967b86e42a87027b2aea67e04ff7
zzucch <nikitamilovatskiy@gmail.com>
2025-02-12 03:25:41 +0300

    fix(kasou): change networking interface

--- hosts/kasou/modules/network.nix
+++ hosts/kasou/modules/network.nix
@@ -1,4 +1,4 @@
 {lib, ...}: {
-  networking.bridges.vmbr0.interfaces = ["ens18"];
+  networking.bridges.vmbr0.interfaces = ["enp7s0"];
   networking.interfaces.vmbr0.useDHCP = lib.mkDefault true;
 }
