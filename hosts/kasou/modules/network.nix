a56ba7ed2cbc85cbb5a6bd0406447dd79c43505d
zzucch <nikitamilovatskiy@gmail.com>
2025-02-13 23:47:38 +0300

    fix(kasou): finally fix network

--- hosts/kasou/modules/network.nix
+++ hosts/kasou/modules/network.nix
@@ -1,22 +1,20 @@
 {
-  systemd.network.networks."10-lan" = {
-    matchConfig.Name = ["enp7s0"];
-    networkConfig = {
-      Bridge = "vmbr0";
+  networking = {
+    bridges.vmbr0.interfaces = ["enp7s0"];
+    interfaces = {
+      vmbr0 = {
+        ipv4.addresses = [
+          {
+            address = "192.168.0.100";
+            prefixLength = 24;
+          }
+        ];
+        useDHCP = false;
+      };
     };
-  };
-  systemd.network.netdevs."vmbr0" = {
-    netdevConfig = {
-      Name = "vmbr0";
-      Kind = "bridge";
-    };
-  };
-  systemd.network.networks."10-lan-bridge" = {
-    matchConfig.Name = "vmbr0";
-    networkConfig = {
-      IPv6AcceptRA = true;
-      DHCP = "ipv4";
-    };
-    linkConfig.RequiredForOnline = "routable";
+    defaultGateway = "192.168.0.1";
+    nameservers = [
+      "192.168.0.1"
+    ];
   };
 }
