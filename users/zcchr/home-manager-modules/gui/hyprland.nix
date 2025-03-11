a320a7ed90e6ffa7e9063c254bf2f80133586a4a
zzucch <nikitamilovatskiy@gmail.com>
2025-03-11 22:37:56 +0300

    feat: turn off workplace switch animation

--- users/zcchr/home-manager-modules/gui/hyprland.nix
+++ users/zcchr/home-manager-modules/gui/hyprland.nix
@@ -109,7 +109,7 @@
           "border, 1, 10, default"
           "borderangle, 1, 8, default"
           "fade, 1, 7, default"
-          "workspaces, 1, 6, default"
+          "workspaces, 0, 6, default"
         ];
       };
 
