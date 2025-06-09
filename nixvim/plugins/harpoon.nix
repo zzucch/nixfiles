23ded98726ee685b224da61f52f382cb064e3aec
zzucch <nikitamilovatskiy@gmail.com>
2025-06-10 02:51:22 +0300

    feat: update dependencies

--- nixvim/plugins/harpoon.nix
+++ nixvim/plugins/harpoon.nix
@@ -1,16 +1,37 @@
 {
-  programs.nixvim.plugins.harpoon = {
-    enable = true;
-    keymapsSilent = true;
-    keymaps = {
-      addFile = "<leader>a";
-      toggleQuickMenu = "<C-e>";
-      navFile = {
-        "1" = "<C-j>";
-        "2" = "<C-k>";
-        "3" = "<C-l>";
-        "4" = "<C-n>";
-      };
-    };
+  programs.nixvim = {
+    plugins.harpoon.enable = true;
+    keymaps = [
+      {
+        mode = "n";
+        key = "<leader>a";
+        action.__raw = "function() require'harpoon':list():add() end";
+      }
+      {
+        mode = "n";
+        key = "<C-e>";
+        action.__raw = "function() require'harpoon'.ui:toggle_quick_menu(require'harpoon':list()) end";
+      }
+      {
+        mode = "n";
+        key = "<C-j>";
+        action.__raw = "function() require'harpoon':list():select(1) end";
+      }
+      {
+        mode = "n";
+        key = "<C-k>";
+        action.__raw = "function() require'harpoon':list():select(2) end";
+      }
+      {
+        mode = "n";
+        key = "<C-l>";
+        action.__raw = "function() require'harpoon':list():select(3) end";
+      }
+      {
+        mode = "n";
+        key = "<C-m>";
+        action.__raw = "function() require'harpoon':list():select(4) end";
+      }
+    ];
   };
 }
