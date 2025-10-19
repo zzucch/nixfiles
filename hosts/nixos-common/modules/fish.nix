9712574312d7f35830e03e440c02e95e16b06466
zzucch <nikitamilovatskiy@gmail.com>
2025-10-20 02:07:38 +0300

    feat: add stuff

--- hosts/nixos-common/modules/fish.nix
+++ hosts/nixos-common/modules/fish.nix
@@ -4,17 +4,21 @@
     shellAbbrs = {
       t = "tmux";
       ta = "tmux a";
+      ts = "tmux-sessionizer";
 
       n = "nvim .";
 
       ga = "git add -A";
       gc = "git commit -m";
+      meow = "git commit -m \"meow\"";
       gca = "git commit --amend";
       gp = "git push";
+      gpf = "git push --force";
       gs = "git status";
       gd = "git diff --color | less";
       gds = "git diff --staged --color | less";
       gdss = "git diff --staged --stat --color | less";
+      gdms = "git diff master --staged --stat --color | less";
       gsc = "git switch -c";
       gsm = "git switch master";
       grs = "git restore --staged .";
@@ -23,17 +27,20 @@
       gb = "git bisect start";
       # NOTE: havent tested the thing in bash; for zsh, use \`
       # NOTE: it might fail when there are multiple root commits
-      gbs = "git bisect start && git bisect bad && git bisect good (git rev-list --max-parents=0 --first-parent HEAD)";
+      gbew = "git bisect start && git bisect bad && git bisect good (git rev-list --max-parents=0 --first-parent HEAD)";
       gbb = "git bisect bad";
       gbg = "git bisect good";
       gbn = "git bisect new";
       gbo = "git bisect old";
-      gbr = "git bisect reset";
+      gbend = "git bisect reset";
 
       gg = "go get";
       gmt = "go mod tidy";
       gt = "go test ./...";
       gtc = "go test -count=1 ./...";
+
+      dsa = "sudo docker stop \$(docker ps -a -q)";
+      dra = "sudo docker rm \$(docker ps -a -q)";
     };
     shellInit = ''
       set fish_greeting
--- users/zcchr/home-manager-modules/common/tmux.nix
+++ users/zcchr/home-manager-modules/common/tmux.nix
@@ -7,12 +7,18 @@
     clock24 = true;
     baseIndex = 1;
     escapeTime = 10;
-    historyLimit = 5000;
+    historyLimit = 100000;
     extraConfig = ''
+      set -g renumber-windows on
+
       set-option -g focus-events on
       set-option -sa terminal-features ',xterm-kitty:RGB'
+
       set -g status-style 'bg=#000000 fg=#302730'
-      set -g renumber-windows on
+      set-option -g status-right '%H:%M %a-%d-%b'
+
+      bind-key -r C-t run-shell "tmux neww tmux-sessionizer"
+      bind -r D neww -c "#{pane_current_path}" "[[ -e TODO.md ]] && nvim TODO.md || nvim ~/TODO.md"
     '';
   };
 }
