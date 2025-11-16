871ec28207c139c49402c31ec9d37cc49af0cd79
zzucch <nikitamilovatskiy@gmail.com>
2025-11-16 21:15:20 +0300

    feat: change the network thing driver

--- hosts/zatsumu/configuration.nix
+++ hosts/zatsumu/configuration.nix
@@ -1,17 +1,25 @@
-{abs, ...}: {
+{
+  abs,
+  config,
+  ...
+}: {
   imports = [
     ./hardware-configuration.nix
     (abs "hosts/nixos-common/configuration.nix")
   ];
   networking.hostName = "zatsumu";
-  boot.loader = {
-    systemd-boot.enable = false;
-    grub = {
-      enable = true;
-      devices = ["nodev"];
-      efiSupport = true;
-      useOSProber = true;
-      splashImage = null;
+  boot = {
+    loader = {
+      systemd-boot.enable = false;
+      grub = {
+        enable = true;
+        devices = ["nodev"];
+        efiSupport = true;
+        useOSProber = true;
+        splashImage = null;
+      };
     };
+    blacklistedKernelModules = ["rtw88_8821cu"];
+    extraModulePackages = with config.boot.kernelPackages; [rtl8821cu];
   };
 }
