8053446de9069d67113f79f11863124b90646b3a
zzucch <nikitamilovatskiy@gmail.com>
2026-03-17 00:48:49 +0300

    awg-quick

--- hosts/nixos-common/modules/amnezia.nix
+++ hosts/nixos-common/modules/amnezia.nix
@@ -1,3 +1,15 @@
 {
+  pkgs,
+  config,
+  ...
+}: {
   programs.amnezia-vpn.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    amneziawg-tools
+  ];
+  boot.extraModulePackages = [
+    config.boot.kernelPackages.amneziawg
+  ];
+  users.users.zcchr.extraGroups = ["amneziawg"];
 }
